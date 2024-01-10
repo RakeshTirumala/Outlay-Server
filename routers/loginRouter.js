5078a672758b8a5e0e00f8cb86dc22026653c0aa
RakeshTirumala <rakeshhtirumala@gmail.com>
2024-01-10 18:27:33 -0500

    made Login server code better implementing jwt token

--- routers/loginRouter.js
+++ routers/loginRouter.js
@@ -3,6 +3,7 @@ const expressAsyncHandler = require('express-async-handler')
 const bcrypt = require('bcrypt');
 const User = require('../models/userModel');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 const userLoginRouter = express.Router();
 
@@ -11,14 +12,19 @@ userLoginRouter.post('/', expressAsyncHandler(async(request, response)=>{
         const email = request.body.email;
         const password = request.body.password;
     
+        //VALIDATING EMAIL AND PASSWORD
         const existingUser = await User.findOne({email});
         if(!existingUser) return response.status(401).json({error:"Invalid email or password!"});
     
         const passwordMatch = await bcrypt.compare(password, existingUser.password);
     
         if(!passwordMatch) return response.status(401).json({error:"Invalid email or password!"});
+
+        //GENERATE TOKEN 
+        const token = jwt.sign({ email: existingUser._id}, process.env.JWT_SECRET_KEY, { expiresIn: '720h' });
     
-        response.status(201).json({message:'Login Successful!'})
+        //RESPONSE
+        response.status(201).json({message:'Login Successful!', JWTtoken:token})
     }catch(error){
         console.log(error)
         response.status(500).json({error:'Internal Error!'});
